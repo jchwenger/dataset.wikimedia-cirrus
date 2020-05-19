a26fd6bd69a74738655718201ddcdd42fd3c7718
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-05-19 21:50:11 +0100

    wiki | tweaks to extract-cirrus (printing)

--- extract-cirrus.sh
+++ extract-cirrus.sh
@@ -8,20 +8,20 @@ echo ""
 echo "extracting cirrus dumps"
 
 DUMP=cirrus
-declare -a NAMES=$(ls $DUMP/*.gz)
+declare -a NAMES=$(ls -Sr $DUMP/*.gz) # starting from smallest
 
 echo ""
 echo_underlined "dumps:"
 echo $NAMES | sed 's/ /\n/g'
+echo ""
 
 echo_underlined "extracting:"
 
 for NAME in ${NAMES[@]}
 do
-  echo_underlined "$NAME"
-  echo_sep
+  echo "$NAME"
 
-  python wikiextractor/cirrusextractor.py $NAME
+  python wikiextractor/cirrusextractor.py -c $NAME
 
 done
 
